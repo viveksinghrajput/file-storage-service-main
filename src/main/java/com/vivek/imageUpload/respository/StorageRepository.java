cccab4acd01a1b39a7feafd2c26eed6b2a305b57
vivek kumar <viveksingh6262@rediffmail.com>
2022-08-30 00:12:41 +0530

    Initial commit

---
+++ src/main/java/com/vivek/imageUpload/respository/StorageRepository.java
@@ -0,0 +1,12 @@
+package com.vivek.imageUpload.respository;
+
+import com.vivek.imageUpload.entity.ImageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StorageRepository extends JpaRepository<ImageData,Long> {
+
+
+    Optional<ImageData> findByName(String fileName);
+}
